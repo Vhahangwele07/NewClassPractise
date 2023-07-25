25a1aad932f5f674d23dcbacf0ba142d652f1ed9
Vhahangwele07 <140509423+Vhahangwele07@users.noreply.github.com>
2023-07-25 15:17:44 +0200

    Add files via upload

---
+++ BasicPractiseFrameApp.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package basicpractiseframeapp;
+
+import za.ac.tut.ui.NewClassPractise;
+
+/**
+ *
+ * @author VHAHANGWELE
+ */
+public class BasicPractiseFrameApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        //declaring variable
+        NewClassPractise myFrame;
+        
+        //instatiating the frame
+        myFrame=new NewClassPractise();
+        
+    }
+    
+}
